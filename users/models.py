ffaee67ee615c348169be7f69d6c9fc6f8e89446
firebreather <dtenny95@gmail.com>
2022-06-15 23:09:46 +0100

    initialization commit

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True,null=True)
+    
